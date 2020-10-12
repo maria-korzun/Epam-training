774096f47a8b47d4ff02b728cf2be6697c76408b
Mariia Korzun <3151020m@gmail.com>
2020-10-12 23:01:11 +0300

    Homework 7 is done.

--- FL14_HW7/homework/js/task1.js
+++ FL14_HW7/homework/js/task1.js
@@ -1,1 +1,25 @@
-// Your code goes here
+const maxPercentage = 100;
+const precision = 2;
+function task1() {
+    const allBatteries = parseFloat(prompt('Please enter amount of batteries'))
+
+    if (!verify(allBatteries)) {
+        alert('Invalid input data')
+        return
+    }
+    const percentOfDefectiveBatteries = parseFloat(prompt('Please enter amount of defective batteries'))
+    if (!verify(percentOfDefectiveBatteries) || percentOfDefectiveBatteries > maxPercentage) {
+        alert('Invalid input data')
+        return
+    }
+    const defectiveBatteries = allBatteries * percentOfDefectiveBatteries / maxPercentage
+    const workingBatteries = allBatteries - defectiveBatteries
+    let message = `Amount of batteries: ${allBatteries.toFixed(precision)}\n`
+    message += `Defective rate: ${percentOfDefectiveBatteries.toFixed(precision)}%\n`
+    message += `Amount of defective batteries: ${defectiveBatteries.toFixed(precision)}\n`
+    message += `Amount of working batteries: ${workingBatteries.toFixed(precision)}`
+    alert(message)
+}
+function verify(prompt) {
+    return !prompt.isNaN && prompt >= 0
+}
--- FL14_HW7/homework/js/task2.js
+++ FL14_HW7/homework/js/task2.js
@@ -1,1 +1,31 @@
-// Your code goes here
+const divisionRatio = 2
+function task2() {
+    const userString = prompt('Please enter a string')
+    if (userString === '') {
+        alert('Invalid value')
+        return
+    }
+    let onlySpaces = true
+    for (let i = 0; i <= userString.length - 1; i++) {
+        if (userString[i] !== ' ') {
+            onlySpaces = false
+            break
+        }
+    }
+    if (onlySpaces) {
+        alert('Invalid value')
+        return
+    }
+    const middleIndex = userString.length / divisionRatio
+    if (userString.length % divisionRatio !== 0) {
+        alert(`${userString[Math.floor(middleIndex)]}`)
+        return
+    }
+    if (userString[middleIndex - 1] !== userString[middleIndex]) {
+        alert(`${userString[middleIndex - 1]}` + `${userString[middleIndex]}`)
+        return
+    }
+    alert('Middle characters are the same')
+}
+
+
