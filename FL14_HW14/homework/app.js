a590edea3a623d1a759b3ca035faff63e0753150
Mariia Korzun <3151020m@gmail.com>
2020-10-29 00:21:47 +0200

    Homework 14 initial commit.

---
+++ FL14_HW14/homework/app.js
@@ -0,0 +1,53 @@
+const data = [
+  {
+    'folder': true,
+    'title': 'Grow',
+    'children': [
+      {
+        'title': 'logo.png'
+      },
+      {
+        'folder': true,
+        'title': 'English',
+        'children': [
+          {
+            'title': 'Present_Perfect.txt'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    'folder': true,
+    'title': 'Soft',
+    'children': [
+      {
+        'folder': true,
+        'title': 'NVIDIA',
+        'children': null
+      },
+      {
+        'title': 'nvm-setup.exe'
+      },
+      {
+        'title': 'node.exe'
+      }
+    ]
+  },
+  {
+    'folder': true,
+    'title': 'Doc',
+    'children': [
+      {
+        'title': 'project_info.txt'
+      }
+    ]
+  },
+  {
+    'title': 'credentials.txt'
+  }
+];
+
+const rootNode = document.getElementById('root');
+
+// TODO: your code goes here
