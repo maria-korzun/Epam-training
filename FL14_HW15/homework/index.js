175e1d410f1a73aeab7a8efa871b272d9ea54041
Mariia Korzun <3151020m@gmail.com>
2020-11-01 04:11:05 +0200

    Homework 15 initial commit

---
+++ FL14_HW15/homework/index.js
@@ -0,0 +1,11 @@
+/* START TASK 1: Your code goes here */
+
+/* END TASK 1 */
+
+/* START TASK 2: Your code goes here */
+
+/* END TASK 2 */
+
+/* START TASK 3: Your code goes here */
+
+/* END TASK 3 */
